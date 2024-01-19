03f6f0d345d2afc2d682b5480fdfe1135fcd6b0d
okazaki418 <okazaki@SX14>
2024-01-19 22:04:11 +0900

    lsp周りの修正

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,5 +1,39 @@
 return {
   {
+    "williamboman/mason.nvim",
+    event = "VeryLazy",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = "BufReadPre",
+    dependencies = {
+      {"williamboman/mason.nvim"},
+      {"neovim/nvim-lspconfig"},
+      {"hrsh7th/cmp-nvim-lsp"}
+    },
+    config = function()
+      local opt = {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      }
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers {
+        function(server_name)
+          require("lspconfig")[server_name].setup(opt)
+        end
+      }
+    end
+  },
+
+  {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
