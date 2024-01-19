26f46451c2108e50ccab57301023e21d64a65644
okazaki418 <okazaki@SX14>
2024-01-20 00:09:26 +0900

    none-lsのインストール

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -31,4 +31,24 @@ return {
       }
     end
   },
+
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = true,
+  },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = {"BufReadPre", "BufNewFile"},
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim"
+    },
+    opts = {
+      handlers = {}
+    },
+  },
 }
