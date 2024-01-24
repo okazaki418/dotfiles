fe2afb587f6d703ca7a7053a8adc6e4fc5aa8186
okazaki418 <okazaki@SX14>
2024-01-24 11:29:02 +0900

    markdownのタブを2に固定

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -10,3 +10,11 @@ optl.comments:append('b:1.')
 
 optl.formatoptions:remove('c')
 optl.formatoptions:append('jro')
+
+-- インデント
+-- -- インデント量
+vim.opt.softtabstop = 2
+-- インデント移動量
+vim.opt.shiftwidth = 2
+-- タブ文字幅
+vim.opt.tabstop = 2
