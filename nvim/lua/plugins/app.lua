149f1363ca352236ad92737145bd4f181ce9088a
okazaki418 <okazaki@SX14>
2024-01-29 17:11:19 +0900

    フォーマット

--- nvim/lua/plugins/app.lua
+++ nvim/lua/plugins/app.lua
@@ -3,9 +3,9 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     opts = {
-      options = {separator_style = "thick"},
+      options = { separator_style = "thick" },
       highlights = {
-        buffer_selected = {italic = false}
+        buffer_selected = { italic = false }
       }
     },
   },
@@ -34,17 +34,17 @@ return {
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", {fg = "#E06C75"})
-        vim.api.nvim_set_hl(0, "RainbowYellow", {fg = "#E5C07B"})
-        vim.api.nvim_set_hl(0, "RainbowBlue", {fg = "#61AFEF"})
-        vim.api.nvim_set_hl(0, "RainbowOrange", {fg = "#D19A66"})
-        vim.api.nvim_set_hl(0, "RainbowGreen", {fg = "#98C379"})
-        vim.api.nvim_set_hl(0, "RainbowViolet", {fg = "#C678DD"})
-        vim.api.nvim_set_hl(0, "RainbowCyan", {fg = "#56B6C2"})
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
 
-      vim.g.rainbow_delimiters = {highlight = highlight}
-      require("ibl").setup {scope = {highlight = highlight}}
+      vim.g.rainbow_delimiters = { highlight = highlight }
+      require("ibl").setup { scope = { highlight = highlight } }
 
       hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end
@@ -66,7 +66,7 @@ return {
         "",
         "<leader>l",
         require("lsp_lines").toggle,
-        {desc = "Toggle lsp_lines"}
+        { desc = "Toggle lsp_lines" }
       )
     end
   },
@@ -92,7 +92,7 @@ return {
 
   {
     "norcalli/nvim-colorizer.lua",
-    event = {"BufRead", "BufNewFile"},
+    event = { "BufRead", "BufNewFile" },
     config = function()
       vim.o.termguicolors = true
       require("colorizer").setup()
