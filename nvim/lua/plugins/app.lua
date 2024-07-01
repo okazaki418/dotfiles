bc87fd22b5a137cdb0e442887c711396f389f7d3
okazaki418 <okazaki@SX14>
2024-07-01 15:53:47 +0900

    fix

--- nvim/lua/plugins/app.lua
+++ nvim/lua/plugins/app.lua
@@ -20,6 +20,7 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "VimEnter",
+    -- lazy = false, priority = 1000,
     config = function()
       local highlight = {
         "RainbowRed",
@@ -30,11 +31,11 @@ return {
         "RainbowViolet",
         "RainbowCyan",
       }
-      local hooks = require("ibl.hooks")
+      local hooks = require "ibl.hooks"
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
         vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
         vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
         vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
