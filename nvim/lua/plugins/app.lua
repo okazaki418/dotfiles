de63219a1185f7d4ceed16b3294c087e54cda3e0
Shintaro Okazaki <okazaki418@outlook.jp>
2024-07-01 17:02:37 +0900

    iblをhlchunk.nvimに置換

--- nvim/lua/plugins/app.lua
+++ nvim/lua/plugins/app.lua
@@ -12,42 +12,75 @@ return {
 
   {
     "j-hui/fidget.nvim",
-    tag = "legacy",
     event = "LspAttach",
-    opts = {}
+    opts = {
+      notification = {
+        window = {
+          winblend = 1,
+        },
+      }
+    }
   },
 
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   event = "VimEnter",
+  --   -- lazy = false, priority = 1000,
+  --   config = function()
+  --     local highlight = {
+  --       "RainbowRed",
+  --       "RainbowYellow",
+  --       "RainbowBlue",
+  --       "RainbowOrange",
+  --       "RainbowGreen",
+  --       "RainbowViolet",
+  --       "RainbowCyan",
+  --     }
+  --     local hooks = require "ibl.hooks"
+  --     -- create the highlight groups in the highlight setup hook, so they are reset
+  --     -- every time the colorscheme changes
+  --     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  --       vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
+  --       vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  --       vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
+  --       vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  --       vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
+  --       vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  --       vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
+  --     end)
+  --
+  --     vim.g.rainbow_delimiters = { highlight = highlight }
+  --     require("ibl").setup { scope = { highlight = highlight } }
+  --
+  --     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+  --   end
+  -- },
   {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "VimEnter",
-    -- lazy = false, priority = 1000,
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      local highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowOrange",
-        "RainbowGreen",
-        "RainbowViolet",
-        "RainbowCyan",
-      }
-      local hooks = require "ibl.hooks"
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
-      end)
-
-      vim.g.rainbow_delimiters = { highlight = highlight }
-      require("ibl").setup { scope = { highlight = highlight } }
-
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          style = {
+            { fg = "#0072bc" },
+            -- { fg = "#1040cf" },
+            { fg = "#cc0033" },
+          },
+          duration = 200,
+          delay = 10,
+        },
+        indent = {
+          enable = true,
+        },
+        line_num = {
+          enable = true,
+          style = {
+            { fg = "#0072bc" },
+          },
+          use_treesitter = true,
+        },
+      })
     end
   },
 
