8b9233c804a7861ea5557b50cfeb5c04c6d753d5
Shintaro Okazaki <okazaki418@outlook.jp>
2024-07-04 12:18:11 +0900

    live_grepのショートカットを追加

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -9,6 +9,7 @@ return {
       require('telescope').setup {}
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
     end
   }
 }
