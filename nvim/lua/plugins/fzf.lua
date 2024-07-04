0f2a987b4941c83c8e6e8eda9f2d2904ccd54ee8
Shintaro Okazaki <okazaki418@outlook.jp>
2024-07-04 11:57:38 +0900

    telescopeの導入

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      require('telescope').setup {}
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+    end
+  }
+}
